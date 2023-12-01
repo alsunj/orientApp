30fa3766c8c192a7cff74e048f9798e937fdaf53
indayz <alex.sunjajev@gmail.com>
2023-12-01 04:33:28 -0800

    register

---
+++ Views/SessionsView.swift
@@ -0,0 +1,18 @@
+//
+//  SessionsView.swift
+//  orientApp
+//
+//  Created by Alex Šunjajev on 01.12.2023.
+//
+
+import SwiftUI
+
+struct SessionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SessionsView()
+}
